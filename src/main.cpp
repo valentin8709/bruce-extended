d0ff00cf92e2338a42abe7bc058e3d66d4c63807
Valentin Dole <valentin.dole@ssi.gouv.fr>
2024-07-23 11:38:10 +0200

    Fix: remove useless deps

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <functional>
 #include <vector>
 #include <string>
-#include <Wire.h>
 #include "esp32-hal-psram.h"
 
 // Public Globals Variables
@@ -40,9 +39,6 @@ uint8_t buff[4096] = {0};
 TFT_eSPI tft = TFT_eSPI();         // Invoke custom library
 TFT_eSprite sprite = TFT_eSprite(&tft);
 TFT_eSprite draw = TFT_eSprite(&tft);
-
-#define I2C_SDA_PIN 21
-#define I2C_SCL_PIN 22
 
 #if defined(CARDPUTER)
   Keyboard_Class Keyboard = Keyboard_Class();
@@ -104,7 +100,6 @@ void setup() {
   sdcardMounted=false;
   wifiConnected=false;
   BLEConnected=false;
-
 
   // Setup GPIOs and stuff
   #if  defined(STICK_C_PLUS2)
@@ -186,7 +181,6 @@ void setup() {
   _tone(5000, 50);
   Program:
   delay(200);
-  Wire1.begin(I2C_SDA_PIN, I2C_SCL_PIN);
   runClockLoop();
 }
 
